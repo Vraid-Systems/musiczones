a39dc906573e2bd0c044eb94dba175a9d5ae3395
Jason Zerbe <jzerbe@vraidsys.com>
2011-08-17 13:37:38 -0500

    added TimerTask for sending out current Zone Controller ping packets, and methods for tracking the Zone Controllers in the multicast group. now just need to build the servlet that formats this information

--- src/multicastmusiccontroller/ProgramConstants.java
+++ src/multicastmusiccontroller/ProgramConstants.java
@@ -14,6 +14,9 @@ public interface ProgramConstants {
     public static final int groupPortInt = 28845;
     public static final int groupTTL = 4; //see http://tldp.org/HOWTO/Multicast-HOWTO-2.html#ss2.3
     public static final int maxByteSize = 65535;
+    //zone controller misc
+    public static final int allNodesPingInterval = 30; //number of seconds between existance notify
+    public static final int allNodesExpireInterval = 15; //seconds before node record allowed to be overwritten
     //jetty web server
     public static final String webAppContextPathStr = "/";
     public static final String webAppDirStr = "../webapp"; //root is the src directory
--- src/zoneserver/ZoneServerLogic.java
+++ src/zoneserver/ZoneServerLogic.java
@@ -5,26 +5,40 @@
 package zoneserver;
 
 import contrib.Mp3Audio;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.HashMap;
+import java.util.Timer;
+import java.util.TimerTask;
 import multicastmusiccontroller.MulticastClient;
+import multicastmusiccontroller.ProgramConstants;
 
 /**
  *
  * @author Jason Zerbe
  */
-public class ZoneServerLogic {
-    
+public class ZoneServerLogic implements ProgramConstants {
+
     protected String global_NodeUUID = null;
     protected String global_NodeName = null;
     protected MulticastClient mcc = null;
-    
+    protected HashMap<String, String> global_NodeInfoMap = null;
+    protected HashMap<String, Calendar> global_NodeExpireMap = null;
+    protected Timer global_Timer = null;
+
     public ZoneServerLogic(String theNodeUUID, String theNodeName) {
+        global_NodeInfoMap = new HashMap<String, String>();
+        global_NodeExpireMap = new HashMap<String, Calendar>();
         global_NodeUUID = theNodeUUID;
         global_NodeName = theNodeName;
         mcc = new MulticastClient();
-        
         System.out.println("ZMS Logic started");
+
+        global_Timer = new Timer();
+        global_Timer.schedule(new AllNodesPingTimerTask(), allNodesPingInterval * 1000);
+        System.out.println("ZMS Logic timed events added");
     }
-    
+
     @Override
     public void finalize() throws Throwable {
         mcc.closeClient();
@@ -39,7 +53,7 @@ public class ZoneServerLogic {
      */
     public boolean processNetworkCommand(String theNetworkCommand) {
         System.err.println("ZMS Logic recieved network comand to process:\n" + theNetworkCommand + "\n");
-        
+
         if (!theNetworkCommand.isEmpty() && theNetworkCommand.contains("\n")) {
             String[] theNetworkCommandArray = theNetworkCommand.split("\n");
             if (theNetworkCommandArray.length == 2) { //single-line command
@@ -56,14 +70,21 @@ public class ZoneServerLogic {
             } else if (theNetworkCommandArray.length == 3) { //2-line command
                 String[] firstLineArray = theNetworkCommandArray[0].split("=");
                 String[] secondLineArray = theNetworkCommandArray[1].split("=");
-                if (firstLineArray[0].equals("zone")
-                        && firstLineArray[1].equals(global_NodeUUID)
-                        && secondLineArray[0].equals("playurl")
-                        && !secondLineArray[1].isEmpty()) { //get the url and play it
-                    doNodePlayUrl(secondLineArray[1]); //notify the group of what is playing
+                if (firstLineArray[0].equals("zone")) {
+                    if (firstLineArray[1].equals(global_NodeUUID)
+                            && secondLineArray[0].equals("playurl")
+                            && !secondLineArray[1].isEmpty()) { //get the url and play it
+                        doNodePlayUrl(secondLineArray[1]); //notify the group of what is playing
 
-                    Mp3Audio aMp3Audio = new Mp3Audio();
-                    aMp3Audio.playURL(secondLineArray[1]);
+                        Mp3Audio aMp3Audio = new Mp3Audio();
+                        aMp3Audio.playURL(secondLineArray[1]);
+                    } else if (!firstLineArray[1].equals(global_NodeUUID)
+                            && secondLineArray[0].equals("name")
+                            && !secondLineArray[1].isEmpty()) { //store in node lookup table
+                        if (!global_NodeInfoMap.containsKey(firstLineArray[1]) || nodeIsExpired(firstLineArray[1])) {
+                            global_NodeInfoMap.put(firstLineArray[1], secondLineArray[1]); //set or update the record
+                        }
+                    }
                 } else {
                     return false;
                 }
@@ -73,8 +94,21 @@ public class ZoneServerLogic {
         } else {
             return false;
         }
-        
+
         return true;
+    }
+
+    /**
+     * returns HashMap<String, String> that contains the current Zone Controller
+     * information of said Zone Controllers that are connected to the multicast
+     * group
+     * 
+     * TODO: perform a deep copy so that unsuspecting people won't screw up internal references
+     * 
+     * @return HashMap<String, String>
+     */
+    public HashMap<String, String> getNodeInfoMap() {
+        return global_NodeInfoMap;
     }
 
     /**
@@ -86,10 +120,43 @@ public class ZoneServerLogic {
                 + "name=" + global_NodeName + "\n";
         mcc.sendNetworkCommand(theResponseStr);
     }
-    
+
+    /**
+     * constructs and sends a datagram that notifies the group that the Zone
+     * Controller is currently playing a certain URL
+     * @param thePlayingUrlStr String
+     */
     private void doNodePlayUrl(String thePlayingUrlStr) {
         String theResponseStr = "zone=" + global_NodeUUID + "\n"
-                + "playurl=" + thePlayingUrlStr + "\n";
+                + "playurl=" + thePlayingUrlStr + "\n"
+                + "status=0\n";
         mcc.sendNetworkCommand(theResponseStr);
+    }
+
+    /**
+     * check if the give node (via UUID) is past the expiration time (TTL)
+     * @param theNodeUUID String
+     * @return boolean - is the node record past expiration?
+     */
+    private boolean nodeIsExpired(String theNodeUUID) {
+        Calendar theCurrentCalendar = new GregorianCalendar();
+        theCurrentCalendar.setTimeInMillis(theCurrentCalendar.getTimeInMillis() - (allNodesExpireInterval * 1000));
+        for (String aNodeUUIDStr : global_NodeExpireMap.keySet()) {
+            if (global_NodeExpireMap.get(aNodeUUIDStr).before(theCurrentCalendar)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * private TimeTask class that is scheduled to notify all the nodes in the
+     * multicast group about the existence of this Zone Controller
+     */
+    private class AllNodesPingTimerTask extends TimerTask {
+
+        public void run() {
+            doNodeUUIDResponse();
+        }
     }
 }
